1ec1ef497e28c64451fbfdea702f837c284bce45
Joshua Heinemann <heineman@ibr.cs.tu-bs.de>
2020-07-28 22:00:56 +0200

    XRA init prep

---
+++ include/xra.h
@@ -0,0 +1,8 @@
+//
+// Created by Joshua Heinemann on 28.07.20.
+// TU-Braunschweig (heineman@ibr.cs.tu-bs.de)
+//
+#ifndef FAASM_XRA_H
+#define FAASM_XRA_H
+
+#endif //FAASM_XRA_H
---
+++ src/core.c
@@ -0,0 +1,6 @@
+//
+// Created by Joshua Heinemann on 28.07.20.
+// TU-Braunschweig (heineman@ibr.cs.tu-bs.de)
+//
+
+#include <xra.h>
