da6935b59c8e15f0ec8733af4892b48a2ae6ce24
Joshua Heinemann <heineman@ibr.cs.tu-bs.de>
2020-07-30 00:28:27 +0200

    XRA: Wasm_Opcode Hashing added

--- include/xra.h
+++ include/xra.h
@@ -8,17 +8,19 @@
 
 #include <sgx.h>
 #include <sgx_report.h>
+#include <sgx_tcrypto.h>
 #include <sgx_utils.h>
 #include <xra_error.h>
 
 typedef struct _report{
     sgx_report_data_t report_data;
+    uint8_t wasm_opcode_hash[SGX_SHA256_HASH_SIZE];
 }xra_report_t;
 
 #ifdef __cplusplus
 extern "C"{
 #endif
-xra_status_t xra_create_report(const sgx_target_info_t* target_info, const sgx_report_data_t* report_data, xra_report_t* xra_report, sgx_report_t* sgx_report);
+xra_status_t xra_create_report(const sgx_target_info_t* target_info, const sgx_report_data_t* report_data, const uint8_t* wasm_opcode, uint32_t wasm_opcode_len, xra_report_t* xra_report, sgx_report_t* sgx_report);
 #ifdef __cplusplus
 };
 #endif
--- src/core.c
+++ src/core.c
@@ -10,7 +10,7 @@
 #ifdef __cplusplus
 extern "C"{
 #endif
-xra_status_t xra_create_report(const sgx_target_info_t* target_info, const sgx_report_data_t* report_data, xra_report_t* xra_report, sgx_report_t* sgx_report){
+xra_status_t xra_create_report(const sgx_target_info_t* target_info, const sgx_report_data_t* report_data, const uint8_t* wasm_opcode, uint32_t wasm_opcode_len, xra_report_t* xra_report, sgx_report_t* sgx_report){
     sgx_status_t sgx_ret_val;
     sgx_report_data_t report_data_xra_hash;
     if(!target_info)
@@ -19,7 +19,14 @@ xra_status_t xra_create_report(const sgx_target_info_t* target_info, const sgx_r
         return XRA_PTR_NOT_INSIDE_ENCLAVE;
     if(report_data)
         memcpy(xra_report->report_data.d,report_data->d,sizeof(sgx_report_data_t));
-    if((sgx_ret_val = sgx_sha256_msg((const uint8_t*)xra_report, sizeof(xra_report_t),(sgx_sha256_hash_t*)&report_data_xra_hash.d)) != SGX_SUCCESS){
+    if(wasm_opcode && !wasm_opcode_len)
+        return XRA_INVALID_WASM_OPCODE_LEN;
+    if(wasm_opcode){
+        if((sgx_ret_val = sgx_sha256_msg(wasm_opcode,wasm_opcode_len,(sgx_sha256_hash_t*)xra_report->wasm_opcode_hash)) != XRA_SUCCESS)
+            return XRA_SET_SGX_ERROR(XRA_SGX_SHA256_HASH_FAILED,sgx_ret_val);
+    } else
+        memset((void*)xra_report->wasm_opcode_hash,0x0, SGX_SHA256_HASH_SIZE * sizeof(uint8_t));
+    if((sgx_ret_val = sgx_sha256_msg((const uint8_t*)xra_report, sizeof(xra_report_t),(sgx_sha256_hash_t*)report_data_xra_hash.d)) != SGX_SUCCESS){
         return XRA_SET_SGX_ERROR(XRA_SGX_SHA256_HASH_FAILED,sgx_ret_val);
     }
     if((sgx_ret_val = sgx_create_report(target_info,&report_data_xra_hash,sgx_report)) != SGX_SUCCESS){
